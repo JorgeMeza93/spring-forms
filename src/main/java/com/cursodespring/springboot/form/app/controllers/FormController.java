4ce727eb084578ebe3e288e17dd567f7e291e3b8
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-06 22:31:34 -0600

    recuperacion y uso de parametros del formulario

--- src/main/java/com/cursodespring/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/cursodespring/springboot/form/app/controllers/FormController.java
@@ -4,15 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cursodespring.springboot.form.app.models.domain.Usuario;
 
 @Controller	
 public class FormController {
 	@GetMapping("/form")
 	public String form(Model model){
+		model.addAttribute("titulo", "Formulario usuarios");
 		return "form";
 	}
-	@PostMapping
-	public String procesar(Model model){
+	@PostMapping("/form")
+	public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email){
+		Usuario usuario = new Usuario();
+		usuario.setUsername(username);
+		usuario.setEmail(email);
+		usuario.setPassword(password);
+		model.addAttribute("titulo", "Resultado formulario");
+		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
 }
